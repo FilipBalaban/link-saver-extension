1f4506e76e08277f780f6d89fe1ca2376081106a
FilipBalaban <100449267+FilipBalaban@users.noreply.github.com>
2022-08-09 12:01:33 +0200

    refactoring code & adding local storage

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-const nameEl = document.querySelector("#name-input-el")
-const urlEl = document.querySelector("#url-input-el")
+const nameInput = document.getElementById("name-input")
+const urlInput = document.getElementById("url-input")
+const saveInputBtn = document.getElementById("save-input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+// const deleteTabBtn = document.getElementById("delete-tab-btn")
+const ulEl = document.getElementById("ul-el") // Unordered List
 
-const saveInputBtn = document.querySelector("#save-input-btn")
-const ulEl = document.querySelector("#ul-el")
+let myNames = []
+let myLinks = []
 
-let nameInput = []
-let urlInput = []
+let namesFromLocalStorage = JSON.parse( localStorage.getItem("Names") )
+let linksFromLocalStorage = JSON.parse( localStorage.getItem("Links") )
 
-let fullInput = {nameInput, urlInput}
 
-// Save input BTN
-saveInputBtn.addEventListener("click", function () {
-    if (nameEl.value === "" && urlEl.value === "") {
-        saveInputBtn.innerHTML = ` Enter Valid Input`
-    } else {
 
-        saveInputBtn.innerHTML = ` SAVE INPUT `
-        // Name Input
-    nameInput.push(nameEl.value)
-    nameEl.value = ""
-
-    // URL Input
-    urlInput.push(urlEl.value)
-    urlEl.value = ""
-
+if (namesFromLocalStorage && linksFromLocalStorage) {
+    myNames = namesFromLocalStorage
+    myLinks = linksFromLocalStorage
     render()
-    }
+}
+
+
+// Save Input btn clicked
+saveInputBtn.onclick = function () {
     
 
-})
+    if (nameInput.value  === ""  || urlInput.value === "") {
+        saveInputBtn.innerHTML = "Enter valid input in both fields"
+
+    } else {
+        saveInputBtn.innerHTML = "SAVE INPUT"
+
+        myNames.push(nameInput.value)
+        nameInput.value = ""
+        localStorage.setItem("Names", JSON.stringify(myNames) )
+
+
+        myLinks.push(urlInput.value)
+        urlInput.value = ""
+        localStorage.setItem("Links", JSON.stringify(myLinks) )
+
+        render()
+    }
+}
 
 // Render
 function render() {
     let listItems = ""
-
-        for (let i = 0; i < nameInput.length; i++) {
-       
-            listItems += `
-                <li>${nameInput}: 
-                <a href='${urlInput}'>${urlInput}</a>
-                </li>
-            `
-            nameInput = []
-            urlInput = []
-            ulEl.innerHTML += listItems
+    for (let i = 0; i < myLinks.length; i++) {
+        listItems += ` <li>${myNames[i]}: 
+        <a href="${myLinks[i]}" target="_blank">   
+        ${myLinks[i]}</a>
+        </li>
+        `
     }
-    
-    
+    ulEl.innerHTML = listItems
+}
+
+
+// Delete Button
+deleteBtn.onclick = function () {
+    localStorage.clear()
+    location.reload()
 }
 
